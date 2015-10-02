49a340416edb5a9bc8315cfb4f70bbaa9160773b
Sachin Naikwadi <sachin.naikwadi.1@gmail.com>
2015-10-02 18:48:57 -0400

    implemented insertion sort function to sort n intergers

---
+++ src/InsertionSort.java
@@ -0,0 +1,32 @@
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int a[] = {8, 2, 4, 9, 3, 6};
+        printArray(a);
+        sort(a);
+        printArray(a);
+    }
+
+    private static void printArray(int a[]) {
+        for (int anA : a) {
+            System.out.print(anA + " ");
+        }
+        System.out.println();
+    }
+
+    private static void sort(int a[]) {
+        for (int i = 1; i < a.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (a[j] < a[j - 1]) {
+                    //swap a[j] & a[j-1]
+                    int temp = a[j];
+                    a[j] = a[j - 1];
+                    a[j - 1] = temp;
+                }
+            }
+        }
+//        return a;
+    }
+}
